fba5874f0b95842856c34e8e9e9e9b7126a7ba69
Mathilde <mathilderenard626@gmail.com>
2023-03-22 13:03:41 +0100

    Correction faute

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openai
 
 if __name__ == '__main__':
-    openai.api_key = "RemplcerCeTexteParLaClefOpenAI"
+    openai.api_key = "RemplacerCeTexteParLaClefOpenAI"
     completion = openai.Completion.create(engine="text-davinci-003", prompt="Quelle heure est il?", max_tokens=1000)
     print(completion.choices[0]['text'])
 

5ec0c7e42874cb6363d2475639403d85d89ed7b1
Mathilde <mathilderenard626@gmail.com>
2023-03-22 13:02:27 +0100

    code python pour API openAi. Ajouter la bonne clef

---
+++ main.py
@@ -0,0 +1,20 @@
+import openai
+
+if __name__ == '__main__':
+    openai.api_key = "RemplcerCeTexteParLaClefOpenAI"
+    completion = openai.Completion.create(engine="text-davinci-003", prompt="Quelle heure est il?", max_tokens=1000)
+    print(completion.choices[0]['text'])
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
